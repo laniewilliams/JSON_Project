2d48cbd74e30f89593fd4f5718640f9da223668c
Lanie Williams <lanie_williams@baylor.edu>
2022-03-16 16:19:51 -0500

    Json work

--- schools.py
+++ schools.py
@@ -25,13 +25,12 @@ outfile = open('readable_univ_data.json','w')
 
 univ_data = json.load(infile)
 json.dump(univ_data, outfile, indent=4)
-
-#MAP 1 ---------------------------------------------------
-
 list_of_univ = univ_data
 list_of_sel_univ = [102,108,107,127,130]
 
-print(len(list_of_univ))
+#MAP 1 ---------------------------------------------------
+
+#print(len(list_of_univ))
 
 enroll, lons, lats, hover_text = [],[],[],[]
 
@@ -46,13 +45,15 @@ for record in list_of_univ:
 
 
 
-print(enroll[:10]) #printing out the first 10 in the list
-print(lons[:10])
-print(lats[:10]) 
+#print(enroll[:10]) #printing out the first 10 in the list
+#print(lons[:10])
+#print(lats[:10]) 
 
 
 from plotly.graph_objs import Scattergeo, Layout #use the uppercase S even though it's not an option
 from plotly import offline
+
+
 
 data = [
     {'type':'scattergeo',
@@ -61,15 +62,79 @@ data = [
     'text': hover_text,
     'marker': {
         'size': [0.001*size for size in enroll], #just spaces in between
-        'color':enroll,
-        'colorscale': 'Viridis',
-        'reversescale':True,
-        'colorbar': {'title': 'Enrollment'}
+    
     },
     }]
 
-my_layout = Layout(title='Global Earthquakes')
+my_layout = Layout(title='Universities with Female Graduation Rate > 50%')
+fig1 = {'data':data,'layout':my_layout}
+offline.plot(fig1,filename='FemaleGradRate.html')
 
-fig = {'data':data,'layout':my_layout}
+# MAP 2 ---------------------------------------------------------------------------
 
-offline.plot(fig,filename='FemaleGradRate.html')
+enroll, lons, lats, hover_text = [],[],[],[]
+
+
+for record in list_of_univ:
+    if record['NCAA']['NAIA conference number football (IC2020)'] in list_of_sel_univ:
+        if record['Percent of total enrollment that are Black or African American (DRVEF2020)'] > 10:
+            enroll.append(record['Total  enrollment (DRVEF2020)'])
+            lons.append(record['Longitude location of institution (HD2020)'])
+            lats.append(record['Latitude location of institution (HD2020)'])
+            hover_text.append([record['instnm'],str(record['Percent of total enrollment that are Black or African American (DRVEF2020)'])+'%'])
+
+
+
+#print(enroll[:10]) #printing out the first 10 in the list
+#print(lons[:10])
+#print(lats[:10]) 
+
+data = [
+    {'type':'scattergeo',
+    'lon':lons,
+    'lat': lats,
+    'text': hover_text,
+    'marker': {
+        'size': [0.001*size for size in enroll], #just spaces in between
+    
+    },
+    }]
+
+my_layout = Layout(title='Universities with Black or African American Enrollment > 10%')
+fig2 = {'data':data,'layout':my_layout}
+offline.plot(fig2,filename='BorAA_Enrollment.html')
+
+# MAP 3 -----------------------------------------------------------------------------------------------
+
+enroll, lons, lats, hover_text = [],[],[],[]
+
+
+for record in list_of_univ:
+    if record['NCAA']['NAIA conference number football (IC2020)'] in list_of_sel_univ:
+        if record['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)'] > 50000:
+            enroll.append(record['Total  enrollment (DRVEF2020)'])
+            lons.append(record['Longitude location of institution (HD2020)'])
+            lats.append(record['Latitude location of institution (HD2020)'])
+            hover_text.append([record['instnm'],str(record['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)'])+'%'])
+
+
+
+#print(enroll[:10]) #printing out the first 10 in the list
+#print(lons[:10])
+#print(lats[:10]) 
+
+
+data = [
+    {'type':'scattergeo',
+    'lon':lons,
+    'lat': lats,
+    'text': hover_text,
+    'marker': {
+        'size': [0.001*size for size in enroll], #just spaces in between
+    
+    },
+    }]
+
+my_layout = Layout(title='Universities with Total Price for In-state Students Living Off-Campus > $50,000')
+fig3 = {'data':data,'layout':my_layout}
+offline.plot(fig3,filename='Instate_offcampus_price.html')
